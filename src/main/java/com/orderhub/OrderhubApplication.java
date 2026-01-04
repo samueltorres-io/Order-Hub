5d2f24629ed0e4bad5ead6bcea97719860c0d305
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-04 19:59:20 -0300

    first commit - init project

---
+++ src/main/java/com/orderhub/OrderhubApplication.java
@@ -0,0 +1,13 @@
+package com.orderhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderhub/OrderhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
