dc2de523e49c100a337eab08e6a2517656ccb85c
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-09 17:12:53 -0300

    Add UserRole and UserRoleId entities with JPA annotations for role management

---
+++ src/main/java/com/orderhub/entity/UserRole.java
@@ -0,0 +1,46 @@
+package com.orderhub.entity;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_roles")
+public class UserRole {
+
+    @EmbeddedId
+    private UserRoleId id = new UserRoleId();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("roleId")
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @CreatedDate
+    @Column(name = "granted_at", nullable = false, updatable = false)
+    private Instant grantedAt;
+
+}
---
+++ src/main/java/com/orderhub/entity/UserRoleId.java
@@ -0,0 +1,20 @@
+package com.orderhub.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleId implements Serializable{
+
+    private UUID userId;
+    private UUID roleId;
+
+}
