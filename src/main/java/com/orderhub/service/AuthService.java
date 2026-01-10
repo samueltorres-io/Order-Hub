963f08f8c88a2f8359e1e4bc0ed9af1c5ddb875f
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-10 20:22:58 -0300

    feat: Adding function for registrate a new user (without tokens pair wet)

--- src/main/java/com/orderhub/service/AuthService.java
+++ src/main/java/com/orderhub/service/AuthService.java
@@ -1,34 +1,24 @@
 package com.orderhub.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.orderhub.dto.auth.request.Register;
 import com.orderhub.dto.auth.response.AuthResponse;
 import com.orderhub.entity.User;
-import com.orderhub.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    @Transactional
+    private AuthResponse create(Register req) {
 
-    private AuthResponse createUser(Register req) {
-        
-        User user = new User();
-
-        user.setUsername(req.username());
-        user.setEmail(req.email());
-        user.setPasswordHash(passwordEncoder.encode(req.password()));
-
-        User savedUser = userRepository.save(user);
+        User savedUser = userService.create(req);
 
         /* Geração de tokens ... */
 
