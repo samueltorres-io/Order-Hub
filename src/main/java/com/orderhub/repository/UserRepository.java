ee79740f64a8bc5d5f9e4813615bb4d485e9ad51
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-10 20:19:17 -0300

    feat: Updating the method for verifying if an account already exists using the sent email

--- src/main/java/com/orderhub/repository/UserRepository.java
+++ src/main/java/com/orderhub/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.orderhub.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.orderhub.entity.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> existsByEmail(String email);
 
 }
