b528b9743c8e939c284057fb556c9fcef223b3d1
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-12 21:21:08 -0300

    feat: Implement security filter chain for authentication and authorization

--- src/main/java/com/orderhub/config/SecurityConfig.java
+++ src/main/java/com/orderhub/config/SecurityConfig.java
@@ -1,12 +1,34 @@
 package com.orderhub.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests( authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST ,"/api/auth/register").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST ,"/api/auth/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.GET, "/api/products/**").permitAll();
+                    authorize.anyRequest().authenticated();
+            })
+            .build();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
