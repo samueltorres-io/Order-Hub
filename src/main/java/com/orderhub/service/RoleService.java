849de0c88948e6cdc846008dab8928ec135c13c2
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-13 21:53:48 -0300

    feat: Refactor RoleService to improve role association logic and error handling

--- src/main/java/com/orderhub/service/RoleService.java
+++ src/main/java/com/orderhub/service/RoleService.java
@@ -1,10 +1,22 @@
 package com.orderhub.service;
 
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import com.orderhub.entity.Role;
+import com.orderhub.entity.User;
+import com.orderhub.entity.UserRole;
+import com.orderhub.exception.AppException;
+import com.orderhub.exception.ErrorCode;
+import com.orderhub.repository.RoleRepository;
+import com.orderhub.repository.UserRepository;
 import com.orderhub.repository.UserRoleRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,21 +24,35 @@ import lombok.RequiredArgsConstructor;
 public class RoleService {
 
     private final UserRoleRepository userRoleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public boolean verifyRole(UUID userId, String roleName) {
         return userRoleRepository.existsByUserIdAndRoleName(userId, roleName);
     }
 
-    /* Associar Permissão (userId, roleName) */
-    public boolean associateRole(UUID userId, String roleName) {
-        /* User existe */
-        /* Role existe */
-        /* Já existe associação: this.verifyRole */
-        /* Cria associação */
+    @Transactional
+    public void associateRole(UUID userId, String roleName) {
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new AppException(ErrorCode.USR_NOT_FOUND, HttpStatus.NOT_FOUND));
+
+        Role role = roleRepository.findByName(roleName)
+            .orElseThrow(() -> new AppException(ErrorCode.INVALID_INPUT /* Evitar dizer que a role não existe */ , HttpStatus.NOT_FOUND));
+        
+        if (this.verifyRole(userId, roleName)) {
+            throw new AppException(ErrorCode.ASSOCIATION_ALREADY_EXISTS, HttpStatus.CONFLICT);
+        }
+
+        UserRole userRole = new UserRole();
+        userRole.setUser(user);
+        userRole.setRole(role);
+
+        userRoleRepository.save(userRole);
     }
 
     /* Desassociar Permissão (userId, roleName) */
-    pubilc boolean unlinkRole(UUID userId, String roleName) {
+    public boolean unlinkRole(UUID userId, String roleName) {
         /* Verifica se a associação realmente existe */
         /* remove */
     }
