da2f7d71b5c0cdcf173511fc9ec148680dfa3abb
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-20 19:19:47 -0300

    feat: Update CreatedResponse DTO to change status type from boolean to ProductStatus for better clarity and validation

--- src/main/java/com/orderhub/dto/product/response/CreatedResponse.java
+++ src/main/java/com/orderhub/dto/product/response/CreatedResponse.java
@@ -3,12 +3,14 @@ package com.orderhub.dto.product.response;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import com.orderhub.enums.ProductStatus;
+
 public record CreatedResponse(
 
     UUID id,
     String name,
     String description,
     BigDecimal price,
-    boolean status
+    ProductStatus status
 
 ) {}
