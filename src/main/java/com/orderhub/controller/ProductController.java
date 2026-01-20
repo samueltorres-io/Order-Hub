d4aed8fa5d1a7b8bfe307e91e4a137d89a65d158
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-20 18:12:21 -0300

    feat: Implement getProducts endpoint in ProductController to support pagination for product retrieval

--- src/main/java/com/orderhub/controller/ProductController.java
+++ src/main/java/com/orderhub/controller/ProductController.java
@@ -1,5 +1,8 @@
 package com.orderhub.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +37,14 @@ public class ProductController {
     @GetMapping("/{name}")
     public ResponseEntity<ProductResponse> getByName(@PathVariable String name) {
         var response = productService.getByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+        @PageableDefault(page = 0, size = 20) Pageable pageable
+    ) {
+        var response = productService.getProducts(pageable);
         return ResponseEntity.ok(response);
     }
 
