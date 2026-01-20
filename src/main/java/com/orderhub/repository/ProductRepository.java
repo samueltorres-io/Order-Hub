65cdf03fbdbe15835f67eb52152ec13492b0e617
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-19 23:28:48 -0300

    feat: Add findByName method to ProductRepository for retrieving products by name

--- src/main/java/com/orderhub/repository/ProductRepository.java
+++ src/main/java/com/orderhub/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.orderhub.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.orderhub.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     boolean existsByNameAndOwnerId(String name, UUID ownerId);
+
+    Optional<Product> findByName(String name);
 
 }
