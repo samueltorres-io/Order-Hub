befd86d16020da2131082360b681bd1c7d066776
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-21 21:38:30 -0300

    refactor: Implement user authentication and retrieval in create method of OrderService for enhanced order processing

--- src/main/java/com/orderhub/service/OrderService.java
+++ src/main/java/com/orderhub/service/OrderService.java
@@ -3,11 +3,40 @@ package com.orderhub.service;
 import org.springframework.stereotype.Service;
 
 import com.orderhub.dto.order.request.CreateOrderRequest;
+import com.orderhub.entity.User;
+import com.orderhub.exception.AppException;
+import com.orderhub.exception.ErrorCode;
+import com.orderhub.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final UserRepository userRepository;
+
     public void create(CreateOrderRequest req) {
+
+        /* Vaildar se o usuário existe */
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new AppException(ErrorCode.UNAUTHORIZED, HttpStatus.UNAUTHORIZED);
+        }
+
+        Jwt jwt = (Jwt) auth.getPrincipal();
+        UUID userId = UUID.fromString(jwt.getSubject());
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new AppException(ErrorCode.USR_NOT_FOUND, HttpStatus.NOT_FOUND));
 
         /* Validar se os produtos existem */
 
