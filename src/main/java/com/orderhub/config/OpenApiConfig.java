7e8e359539ad4ee6467b58572f4aef76bcca255c
samueltorres-io <torresfernandessamuel@gmail.com>
2026-01-29 15:57:14 -0300

    feat: Add springdoc-openapi dependency for enhanced API documentation support

---
+++ src/main/java/com/orderhub/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.orderhub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("OrderHub API")
+                .version("1.0")
+                .description("Documentação da API de Gestão de Pedidos")
+                .contact(new Contact()
+                    .name("Seu Nome")
+                    .email("seu.email@exemplo.com")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
